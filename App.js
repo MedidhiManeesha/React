376cadd1beab120209356a4b0f9432f53c6614e3
Maneesha <medidhimaneesha@gmail.com>
2024-03-11 22:45:10 +0530

    added resObj

--- App.js
+++ App.js
@@ -38,13 +38,117 @@ const JewelsCard = ({orName, price}) =>{
         </div>
     );
 };
+
+const resObj = {
+    "info": {
+      "id": "185544",
+      "name": "NIC Ice Creams",
+      "cloudinaryImageId": "85825a6d74b1059a63a9b688de9f67ce",
+      "locality": "Khairatabad",
+      "areaName": "Banjara Hills",
+      "costForTwo": "₹120 for two",
+      "cuisines": [
+        "Desserts",
+        "Ice Cream"
+      ],
+      "avgRating": 4.6,
+      "veg": true,
+      "parentId": "6249",
+      "avgRatingString": "4.6",
+      "totalRatingsString": "5K+",
+      "promoted": true,
+      "adTrackingId": "cid=11858919~p=1~eid=0000018e-2e76-6c2e-08ba-eee40031010b~srvts=1710176496686~45995",
+      "sla": {
+        "deliveryTime": 38,
+        "lastMileTravel": 6.1,
+        "serviceability": "SERVICEABLE",
+        "slaString": "35-40 mins",
+        "lastMileTravelString": "6.1 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2024-03-12 02:00:00",
+        "opened": true
+      },
+      "badges": {
+        "textExtendedBadges": [
+          {
+            "iconId": "guiltfree/GF_Logo_android_3x",
+            "shortDescription": "brand",
+            "fontColor": "#7E808C"
+          }
+        ]
+      },
+      "isOpen": true,
+      "aggregatedDiscountInfoV2": {
+        
+      },
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "textBased": {
+            
+          },
+          "imageBased": {
+            
+          },
+          "textExtendedBadges": {
+            "badgeObject": [
+              {
+                "attributes": {
+                  "description": "",
+                  "shortDescription": "brand",
+                  "fontColor": "#7E808C",
+                  "iconId": "guiltfree/GF_Logo_android_3x"
+                }
+              }
+            ]
+          }
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "--"
+        }
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      
+    },
+    "cta": {
+      "link": "swiggy://menu?restaurant_id=185544",
+      "text": "RESTAURANT_MENU",
+      "type": "DEEPLINK"
+    }
+  }
+
 const Body = () => {
     return (
     <div className="body">
             <div className="search">Search</div>
             <div className="jewel-container">
                 <JewelsCard orName="Bangles" price="4000"/>
-                <JewelsCard orName="earrings"/>
+                <JewelsCard orName="earrings" price="5000"/>
             </div>
     </div>
     );
