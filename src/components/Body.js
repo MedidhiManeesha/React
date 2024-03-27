cb1469d4277f9a9bc19314afff563cef1f9e4265
Maneesha <medidhimaneesha@gmail.com>
2024-03-27 22:01:49 +0530

    added new

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,9 +1,22 @@
 import JewelsCard from '../components/JewelsCard';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import resList from '../utils/mockData';
 
 const Body = () => {
 let [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
+useEffect(()=>{
+    fetchData();
+},[]);
+
+const fetchData = async () =>{
+    const data = await fetch(
+        'https://www.swiggy.com/dapi/restaurants/list/v5?lat=17.37240&lng=78.43780&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING'
+    );
+    const json = await data.json();
+    console.log(json);
+}
+
 return (
     <div className="body">
             <div className="filter">
